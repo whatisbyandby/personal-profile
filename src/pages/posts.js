2162ddd8c14c5cee1c27a26654792b0b6abb9b22
Scott Perkins <scottperkins@Scotts-MacBook-Pro.local>
2020-03-21 13:41:36 -0600

    Trying to move layout back to lower case

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -2,7 +2,7 @@ import React from "react"
 import SEO from "../components/seo"
 import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import PostCard from "../components/PostCard"
 
 const CardContainer = styled.div`
