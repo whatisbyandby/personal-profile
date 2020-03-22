4560c5cc51493f9e1b5e6c80d0b10de2afa1ff67
Scott Perkins <scottperkins@Scotts-MacBook-Pro.local>
2020-03-22 16:37:48 -0600

    Added console.log for email address

--- src/functions/sendEmail.js
+++ src/functions/sendEmail.js
@@ -5,6 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 exports.handler = async function(event, context) {
   console.log("Sending Email")
+  console.log(process.env.EMAIL_ADDRESS)
   const data = JSON.parse(event.body)
   const cleanName = xss(data.name)
   const cleanEmail = xss(data.email)
