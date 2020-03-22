4e47c8f59f2c76235ba618f045d7c8f47ee8f70c
Scott Perkins <scottperkins@Scotts-MacBook-Pro.local>
2020-03-22 13:42:23 -0600

    Went back to the old path

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,17 @@
+const proxy = require("http-proxy-middleware")
+
 module.exports = {
+  developMiddleware: app => {
+    app.use(
+      "/.netlify/functions/",
+      proxy({
+        target: "http://localhost:9000",
+        pathRewrite: {
+          "/.netlify/functions/": "",
+        },
+      })
+    )
+  },
   siteMetadata: {
     title: `Scott Perkins`,
     description: `Personal Profile for Scott Perkins`,
