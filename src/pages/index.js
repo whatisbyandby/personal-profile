eefb4cd52f014b6de3f289cbf5e8519196ac7c53
Scott Perkins <scott.perkins@protonmail.com>
2020-10-11 18:44:23 -0600

    Testing that everything still works, starting to add content to replace the lorem ipsum

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,51 +9,7 @@ export default function AboutPage() {
       <SEO title="Home" />
       <div className="content-container">
         <h1>Home</h1>
-        <p>
-          Stinking bishop queso brie. Who moved my cheese roquefort everyone
-          loves the big cheese hard cheese everyone loves squirty cheese
-          ricotta. Cheddar hard cheese bocconcini who moved my cheese fromage
-          frais brie cauliflower cheese cheese and biscuits. Cut the cheese
-          roquefort fondue lancashire bocconcini rubber cheese halloumi squirty
-          cheese. Brie roquefort blue castello danish fontina fondue who moved
-          my cheese airedale pecorino. Paneer cauliflower cheese red leicester
-          bocconcini cheddar cheddar. Halloumi manchego dolcelatte. Pepper jack
-          the big cheese halloumi cheesy grin queso jarlsberg cheesy grin
-          dolcelatte. Fondue airedale st. agur blue cheese dolcelatte croque
-          monsieur rubber cheese say cheese pecorino. Cheesecake pepper jack
-          cheese triangles ricotta jarlsberg parmesan the big cheese cream
-          cheese. Manchego gouda swiss cheese slices melted cheese stinking
-          bishop swiss roquefort. Port-salut danish fontina brie emmental cheesy
-          feet red leicester mozzarella fromage frais. Cheesy grin dolcelatte
-          caerphilly cottage cheese say cheese squirty cheese cheese strings
-          fondue. Airedale feta port-salut cheese triangles taleggio cow
-          stinking bishop when the cheese comes out everybody's happy. Stilton.
-          Red leicester roquefort chalk and cheese. Cheesy feet cauliflower
-          cheese ricotta stilton cow cheese on toast queso cheese triangles.
-          Hard cheese fromage frais smelly cheese cheese triangles cut the
-          cheese edam brie when the cheese comes out everybody's happy. Manchego
-          cow cut the cheese parmesan roquefort cow paneer danish fontina.
-          Cheeseburger bocconcini manchego say cheese cow blue castello cheese
-          slices st. agur blue cheese. Paneer jarlsberg fromage frais babybel
-          feta blue castello jarlsberg jarlsberg. Roquefort cheese and biscuits
-          cream cheese feta parmesan queso cheese on toast cut the cheese.
-          Cauliflower cheese macaroni cheese cheese slices cheese triangles
-          babybel who moved my cheese. Caerphilly squirty cheese jarlsberg.
-          Stilton bocconcini fondue macaroni cheese stinking bishop ricotta
-          cheese triangles stinking bishop. Taleggio feta port-salut parmesan
-          mozzarella pecorino melted cheese boursin. Lancashire croque monsieur
-          port-salut caerphilly gouda fromage mozzarella brie. Port-salut
-          cheesecake cheese triangles melted cheese cream cheese gouda cheese
-          and wine who moved my cheese. Blue castello bavarian bergkase
-          cauliflower cheese cow port-salut swiss. Cheese triangles camembert de
-          normandie hard cheese. Stilton cheesecake jarlsberg port-salut hard
-          cheese cow the big cheese mozzarella. Mozzarella cheese slices
-          halloumi monterey jack squirty cheese squirty cheese boursin
-          cheeseburger. Cheesy feet fondue swiss roquefort edam cheese and
-          biscuits cheesecake camembert de normandie. Taleggio monterey jack
-          monterey jack cottage cheese bocconcini cheeseburger roquefort cheese
-          triangles. Fromage brie pepper jack.
-        </p>
+        <p>This is to do some testing, No more loreum ipsum</p>
       </div>
     </Layout>
   )
